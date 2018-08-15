8ea74ae5acf5326358b761e47b92bdeea2470eff
Will Lawrence <will@bosonmedia.net>
2018-08-15 16:01:56 +0100

    PH - Search results page

--- wp-content/themes/prospect-hospice/template-parts/content-search.php
+++ wp-content/themes/prospect-hospice/template-parts/content-search.php
@@ -1,11 +1,10 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('search-listing-item'); ?>>
+	<div class="search-listing-content">
+		<header class="entry-header">
+			<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		</header><!-- .entry-header -->
 
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-	</header><!-- .entry-header -->
-
-	<?php theme_get_post_thumbnail(); ?>
-
-	<?php the_excerpt(); ?>
+		<?php the_excerpt(); ?>
+	</div>
+	<a href="#" class="btn btn-arrow-right">Read more</a>
 </article>
-
