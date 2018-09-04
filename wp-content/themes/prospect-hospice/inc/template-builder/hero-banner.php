d92cf1b947c04b58cb66ab6ef214df1ff0516474
Thomas Beavan <tom@bosonmedia.net>
2018-09-04 16:48:05 +0100

    Prospect Hospice - Tweaks after testing

--- wp-content/themes/prospect-hospice/inc/template-builder/hero-banner.php
+++ wp-content/themes/prospect-hospice/inc/template-builder/hero-banner.php
@@ -2,6 +2,8 @@
 $image = (get_sub_field('background_image') ? get_sub_field('background_image') : get_field('hero_banner_image'));
 $heading = (get_sub_field('heading') ? get_sub_field('heading') : get_field('hero_banner_heading'));
 $second_heading = (get_sub_field('second_heading') ? get_sub_field('second_heading') : get_field('hero_banner_second_heading'));
+$description = (get_sub_field('description') ? get_sub_field('description') : get_field('hero_banner_description'));
+$link_url = (get_sub_field('link_url') ? get_sub_field('link_url') : get_field('hero_banner_link_url'));
 ?>
 <div class="hero-banner block" <?php if($image) echo 'style="background-image:url(' . $image . ');"'; ?>>
     <div class="overlay"></div>
@@ -15,9 +17,16 @@ $second_heading = (get_sub_field('second_heading') ? get_sub_field('second_headi
                     <?php if($heading) : ?>
                         <h1><?php echo $heading; ?></h1>
                     <?php endif; ?>
-
+                    <?php if($description) : ?>
+                        <p><?php echo $description; ?></p>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
     <?php endif;?>
+
+    <?php if($link_url) : ?>
+        <a class="btn-circle" href="<?php echo $link_url;?>"><i class="fal fa-arrow-right"></i></a>
+    <?php endif; ?>
+
 </div>
