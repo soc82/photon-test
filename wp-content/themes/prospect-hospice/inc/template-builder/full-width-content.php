da17dd61b5bdd82e49e475ccb460bfee7e541657
Ashley Coles <ashley.coles@bosonweb.net>
2018-09-05 15:41:27 +0100

    Updating full width content block

--- wp-content/themes/prospect-hospice/inc/template-builder/full-width-content.php
+++ wp-content/themes/prospect-hospice/inc/template-builder/full-width-content.php
@@ -3,11 +3,11 @@ $heading = get_sub_field('heading');
 $content = get_sub_field('content');
 ?>
 
-<div class="full-wdith-content block" >
+<div class="full-wiith-content block" >
 	<div class="container">
 		<div class="row">
 			<div class="col-12 ">
-					<?php if($heading) echo '<h3>' . $heading . '</h3>' ?>
+					<?php if($heading) echo '<h2>' . $heading . '</h2>' ?>
 					<?php if($content) echo '<p>' . $content . '</p>'; ?>
 			</div>
 		</div>
