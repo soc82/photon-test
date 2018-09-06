53b42dafb745fe404e44c6fc0f508fe115787a0f
Ashley Coles <ashley.coles@bosonweb.net>
2018-09-06 10:07:50 +0100

    Tweaks from testing

--- wp-content/themes/prospect-hospice/inc/template-builder/full-width-content.php
+++ wp-content/themes/prospect-hospice/inc/template-builder/full-width-content.php
@@ -1,14 +1,32 @@
 <?php
 $heading = get_sub_field('heading');
 $content = get_sub_field('content');
+$below_hero_banner = get_sub_field('below_hero_banner');
+$heading_colour = get_sub_field('heading_colour');
 ?>
 
-<div class="full-wiith-content block" >
+<div class="full-width-content block" >
 	<div class="container">
 		<div class="row">
 			<div class="col-12 ">
-					<?php if($heading) echo '<h2>' . $heading . '</h2>' ?>
-					<?php if($content) echo '<p>' . $content . '</p>'; ?>
+				<?php if($heading) : ?>
+					<h2 <?php if ($below_hero_banner) echo "class='below_hero'"; ?>
+					<?php if ($heading_colour) echo 'style="background: ' . $heading_colour . '"'; ?>
+					>
+						<?php echo $heading; ?>
+							
+						</h2>
+				<?php endif; ?>
+
+				<?php if ($below_hero_banner) : ?>
+					<div class="content-below-hero">
+				<?php endif; ?>
+
+				<?php echo $content; ?>
+				
+				<?php if ($below_hero_banner) : ?>
+					</div>
+				<?php endif; ?>
 			</div>
 		</div>
 	</div>
