8647f6cb52e766e7f2a7a56f941b5242a12c3328
Will Lawrence <will@bosonmedia.net>
2018-09-17 15:39:27 +0100

    PH - Donation form update

--- wp-content/themes/prospect-hospice/page.php
+++ wp-content/themes/prospect-hospice/page.php
@@ -1,18 +1,5 @@
-<?php get_header(); ?>
+<?php get_header();
 
-<div class="inner-page-wrapper">
+get_template_part('inc/flexible-content');
 
-	<?php get_template_part('inc/flexible-content'); ?>
-
-	<div class="container">
-		<div class="row">
-			<div class="col-12">
-				<?php while ( have_posts() ) : the_post(); ?>
-					<?php echo the_content();?>
-				<?php endwhile; ?>
-			</div>
-		</div>
-	</div>
-</div>
-
-<?php get_footer(); ?>
+get_footer(); ?>
