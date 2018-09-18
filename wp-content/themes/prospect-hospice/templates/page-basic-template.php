3daf8ca71a978c9697d6f19d05c2017a5e0b2fae
Will Lawrence <will@bosonmedia.net>
2018-09-18 11:21:06 +0100

    PH - Testing updates

--- wp-content/themes/prospect-hospice/templates/page-basic-template.php
+++ wp-content/themes/prospect-hospice/templates/page-basic-template.php
@@ -8,7 +8,7 @@ get_header(); ?>
 <div class="block">
   <div class="container">
     <div class="row">
-      <div class="col-12 col-md-10 offset-md-1">
+      <div class="col-12">
         <?php the_content(); ?>
       </div>
     </div>
