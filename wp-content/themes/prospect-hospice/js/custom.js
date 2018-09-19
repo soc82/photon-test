fbd127232000386f071292f18b2b42902d4df1b6
Will Lawrence <will@bosonmedia.net>
2018-09-19 13:50:55 +0100

    PH - Accessibility updates

--- wp-content/themes/prospect-hospice/js/custom.js
+++ wp-content/themes/prospect-hospice/js/custom.js
@@ -36,10 +36,6 @@ jQuery( document ).ready(function($) {
   new mlPushMenu( document.getElementById( 'mobile-menu' ), document.getElementById( 'trigger' ) );
 
 
-  /*
-  ** Lightbox image gallery
-  */
-  new LuminousGallery(document.querySelectorAll(".lightbox-gallery"));
 
   // Top Menu Search
 	jQuery( ".menu-search-form input" ).click(function() {
