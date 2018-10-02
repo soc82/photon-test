e06388f16e2a79a6404bffc2f7b2f905a6710b52
Thomas Beavan <tom@bosonmedia.net>
2018-10-02 13:50:13 +0100

    Prospect Hospice - Documents block

--- wp-content/themes/prospect-hospice/templates/page-downloads.php
+++ wp-content/themes/prospect-hospice/templates/page-downloads.php
@@ -43,20 +43,20 @@ get_header(); ?>
 						while($documents->have_posts()): $documents->the_post();
 						$file = get_field('file', get_the_ID());
 						if($file): ?>
-								<div class="col-12 col-md-10 offset-md-1 download-item">
-									<div class="row d-block">
-										<div class="col-2 col-sm-1 col-md-1 inline-block-replace-middle">
-											<div class="download-icon"><i class="far <?php echo prospect_file_type_icon(get_the_ID()); ?>"></i></div>
-										</div>
-										<div class="col-10 col-sm-11 col-md-7 col-lg-8 inline-block-replace-middle">
-											<h3><?php echo $post->post_title; ?></h3>
-										</div>
-										<div class="col-10 offset-2 col-sm-11 offset-sm-1 col-md-4 offset-md-0  col-lg-3 inline-block-replace-middle">
-											<a class="btn btn-arrow-right" href="<?php echo $file['url']; ?>" download target="_blank">Download</a>
-										</div>
+							<div class="col-12 col-md-10 offset-md-1 download-item">
+								<div class="row d-block">
+									<div class="col-2 col-sm-1 col-md-1 inline-block-replace-middle">
+										<div class="download-icon"><i class="far <?php echo prospect_file_type_icon(get_the_ID()); ?>"></i></div>
+									</div>
+									<div class="col-10 col-sm-11 col-md-7 col-lg-8 inline-block-replace-middle">
+										<h3><?php echo $post->post_title; ?></h3>
+									</div>
+									<div class="col-10 offset-2 col-sm-11 offset-sm-1 col-md-4 offset-md-0  col-lg-3 inline-block-replace-middle">
+										<a class="btn btn-arrow-right" href="<?php echo $file['url']; ?>" download target="_blank">Download</a>
 									</div>
 								</div>
-				<?php endif;
+							</div>
+						<?php endif;
 						endwhile;
 			    $total_pages = $documents->max_num_pages;
 			    if ($total_pages > 1) : ?>
