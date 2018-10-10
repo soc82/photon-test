393773397f8b8117929b87653a5f47ba40c858d0
Thomas Beavan <tom@bosonmedia.net>
2018-10-10 10:01:54 +0100

    Prospect Hospice - Hero banner validation

--- wp-content/themes/prospect-hospice/inc/template-builder/hero-banner.php
+++ wp-content/themes/prospect-hospice/inc/template-builder/hero-banner.php
@@ -7,23 +7,23 @@ $link_url = (get_sub_field('link_url') ? get_sub_field('link_url') : get_field('
 ?>
 <div class="hero-banner block" <?php if($image) echo 'style="background-image:url(' . $image . ');"'; ?>>
     <div class="overlay"></div>
-    <?php if($heading || $second_heading): ?>
-        <div class="container hero-banner-content">
-            <div class="row">
-                <div class="col-12">
-                    <?php if ($second_heading) : ?>
-                        <h2 class="color-yellow"><?php echo $second_heading; ?></h2>
-                    <?php endif; ?>
-                    <?php if($heading) : ?>
-                        <h1><?php echo $heading; ?></h1>
-                    <?php endif; ?>
-                    <?php if($description) : ?>
-                        <p><?php echo $description; ?></p>
-                    <?php endif; ?>
-                </div>
+    
+    <div class="container hero-banner-content">
+        <div class="row">
+            <div class="col-12">
+                <?php if ($second_heading) : ?>
+                    <h2 class="color-yellow"><?php echo $second_heading; ?></h2>
+                <?php endif; ?>
+                <?php if($heading) : ?>
+                    <h1><?php echo $heading; ?></h1>
+                <?php endif; ?>
+                <?php if($description) : ?>
+                    <p><?php echo $description; ?></p>
+                <?php endif; ?>
             </div>
         </div>
-    <?php endif;?>
+    </div>
+    
 
     <?php if($link_url) : ?>
         <a class="btn-circle" href="<?php echo $link_url;?>"><i class="fal fa-arrow-right"></i></a>
