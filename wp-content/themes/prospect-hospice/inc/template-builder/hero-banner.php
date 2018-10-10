c73ea51ff709ff73cef5a965c6390d8cbd6414e9
Thomas Beavan <tom@bosonmedia.net>
2018-10-10 10:06:19 +0100

    Prospect Hospice - Hero banner min height

--- wp-content/themes/prospect-hospice/inc/template-builder/hero-banner.php
+++ wp-content/themes/prospect-hospice/inc/template-builder/hero-banner.php
@@ -5,28 +5,30 @@ $second_heading = (get_sub_field('second_heading') ? get_sub_field('second_headi
 $description = (get_sub_field('description') ? get_sub_field('description') : get_field('hero_banner_description'));
 $link_url = (get_sub_field('link_url') ? get_sub_field('link_url') : get_field('hero_banner_link_url'));
 ?>
-<div class="hero-banner block" <?php if($image) echo 'style="background-image:url(' . $image . ');"'; ?>>
-    <div class="overlay"></div>
-    
-    <div class="container hero-banner-content">
-        <div class="row">
-            <div class="col-12">
-                <?php if ($second_heading) : ?>
-                    <h2 class="color-yellow"><?php echo $second_heading; ?></h2>
-                <?php endif; ?>
-                <?php if($heading) : ?>
-                    <h1><?php echo $heading; ?></h1>
-                <?php endif; ?>
-                <?php if($description) : ?>
-                    <p><?php echo $description; ?></p>
-                <?php endif; ?>
+
+<?php if(isset($image)):?>
+    <div class="hero-banner block" <?php if($image) echo 'style="background-image:url(' . $image . ');"'; ?>>
+        <div class="overlay"></div>
+        
+        <div class="container hero-banner-content">
+            <div class="row">
+                <div class="col-12">
+                    <?php if ($second_heading) : ?>
+                        <h2 class="color-yellow"><?php echo $second_heading; ?></h2>
+                    <?php endif; ?>
+                    <?php if($heading) : ?>
+                        <h1><?php echo $heading; ?></h1>
+                    <?php endif; ?>
+                    <?php if($description) : ?>
+                        <p><?php echo $description; ?></p>
+                    <?php endif; ?>
+                </div>
             </div>
         </div>
+
+        <?php if($link_url) : ?>
+            <a class="btn-circle" href="<?php echo $link_url;?>"><i class="fal fa-arrow-right"></i></a>
+        <?php endif; ?>
+
     </div>
-    
-
-    <?php if($link_url) : ?>
-        <a class="btn-circle" href="<?php echo $link_url;?>"><i class="fal fa-arrow-right"></i></a>
-    <?php endif; ?>
-
-</div>
+<?php endif;?>
