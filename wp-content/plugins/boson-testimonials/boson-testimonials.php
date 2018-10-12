748c24510667d92f532b71a51f3065a804ed3a59
Ben Lumley <ben@benlumley.co.uk>
2018-10-12 10:58:49 +0100

    No testimonials in search

--- wp-content/plugins/boson-testimonials/boson-testimonials.php
+++ wp-content/plugins/boson-testimonials/boson-testimonials.php
@@ -49,7 +49,7 @@ function custom_post_type_testimonials() {
     'menu_icon'           => 'dashicons-format-status',
     'can_export'          => true,
     'has_archive'         => true, // index Page
-    'exclude_from_search' => false,
+    'exclude_from_search' => true,
     'publicly_queryable'  => true,
     'rewrite'               => $rewrite,
     'capability_type'     => 'page',
