f7bf3d41368ad5c81f65af5f79669c9f37bbe284
Ashley Coles <ashley.coles@bosonweb.net>
2018-10-22 17:22:07 +0100

    Updating hidden admin menu items

--- wp-content/themes/prospect-hospice/functions-custom.php
+++ wp-content/themes/prospect-hospice/functions-custom.php
@@ -87,7 +87,9 @@ function wpse28782_remove_menu_items() {
         remove_menu_page('upload.php'); // Media
         remove_menu_page('link-manager.php'); // Links
         remove_menu_page('edit-comments.php'); // Comments
-        remove_menu_page('edit.php?post_type=page'); // Pages
+        if (!current_user_can( 'volunteering' )) {
+            remove_menu_page('edit.php?post_type=page'); // Pages
+        }
         remove_menu_page('plugins.php'); // Plugins
         remove_menu_page('themes.php'); // Appearance
         remove_menu_page('users.php'); // Users
