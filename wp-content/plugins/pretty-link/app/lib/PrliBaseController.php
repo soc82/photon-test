4c62e5fa7614cbf4645b788611c2f3334e6c9dc5
Will Lawrence <will@bosonmedia.net>
2018-10-30 09:41:04 +0000

    Pretty urls plugin

--- wp-content/plugins/pretty-link/app/lib/PrliBaseController.php
+++
@@ -1,6 +0,0 @@
-<?php if(!defined('ABSPATH')) { die('You are not allowed to call this page directly.'); }
-
-abstract class PrliBaseController {
-  abstract public function load_hooks();
-}
-
--- wp-content/plugins/pretty-link/js/admin_shared.js
+++
@@ -1,8 +0,0 @@
-function toggle_iphone_instructions() {
-  jQuery('.iphone_instructions').slideToggle();
-}
-
-jQuery(document).ready(function($) {
-  $(".prli-social-button-checkboxes").sortable();
-});
-
